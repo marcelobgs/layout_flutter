5c7e0a64dea85faac86012578b25eeb4242a7b66
Marcelo B <marcelobarros159@gmail.com>
2022-05-20 14:19:44 -0300

    adicionada textos com fontes personalizadas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,9 +21,26 @@ class MyApp extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           // ignore: prefer_const_literals_to_create_immutables
           children: [
-            CircleAvatar(
-              backgroundImage: AssetImage('./assets/images/usuario.jpg'),
-              radius: 100,
+            Container(
+              margin: EdgeInsets.zero,
+              child: CircleAvatar(
+                backgroundImage: AssetImage('./assets/images/usuario.png'),
+                backgroundColor: Colors.white,
+                radius: 100,
+              ),
+            ),
+            Text(
+              'Greek Freak',
+              style: TextStyle(fontSize: 30, fontFamily: 'Koulen'),
+            ),
+            Text(
+              'Giannis Sina Ugo Antetokounmpo',
+              style: TextStyle(
+                  color: Colors.white, fontSize: 25, fontFamily: 'Oranienbaum'),
+            ),
+            Text(
+              'Γιάννης Σίνα Ούγκο Αντετοκούνμπο',
+              style: TextStyle(fontSize: 20, fontFamily: 'GFSDidot'),
             ),
           ],
         ),
